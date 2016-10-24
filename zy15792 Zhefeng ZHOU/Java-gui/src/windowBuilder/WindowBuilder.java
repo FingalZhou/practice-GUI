56b70b33b0b714b8dc204642bed2dd9dfa22248e
Fingal <Fingal.ZHOU@hotmail.com>
2016-10-24 12:33:52 +0800

    creating 2 new folder for new framework

--- zy15792 Zhefeng ZHOU/Java-gui/src/windowBuilder/WindowBuilder.java
+++ zy15792 Zhefeng ZHOU/Java-gui/src/windowBuilder/WindowBuilder.java
@@ -1,9 +1,13 @@
 package windowBuilder;
 
+
+import java.util.*;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
@@ -19,10 +23,18 @@ import java.awt.event.ActionEvent;
 import javax.swing.JSplitPane;
 import java.awt.FlowLayout;
 import net.miginfocom.swing.MigLayout;
+import java.awt.CardLayout;
+import javax.swing.JToolBar;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.AbstractAction;
 
 public class WindowBuilder extends JFrame {
-
-	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -48,25 +60,36 @@ public class WindowBuilder extends JFrame {
 		setFont(new Font("Calibri", Font.PLAIN, 12));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 674, 300);
-		contentPane = new JPanel();
-		contentPane.setToolTipText("");
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[57px][67px][75px][][][][][][][][][][][]", "[25px][][][][][][]"));
 		
-		JButton btnConfirm = new JButton("confirm");
-		btnConfirm.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		textField = new JTextField();
+		textField.setColumns(10);
 		
-		JCheckBox chckbxMale = new JCheckBox("male");
-		contentPane.add(chckbxMale, "cell 2 2,alignx left,aligny top");
+		JButton btnReverse = new JButton("reverse");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(70)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
+					.addGap(65)
+					.addComponent(btnReverse)
+					.addContainerGap(296, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(94)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField)
+						.addComponent(btnReverse))
+					.addGap(137))
+		);
+		getContentPane().setLayout(groupLayout);
 		
-		JCheckBox chckbxFemale = new JCheckBox("female");
-		contentPane.add(chckbxFemale, "cell 2 4,alignx left,aligny top");
-		contentPane.add(btnConfirm, "cell 13 6,alignx left,aligny top");
 	}
-
+	
+	public String Reverse(String str) {
+		String rev = new StringBuffer(str).reverse().toString();
+		return rev;
+	}
 }
